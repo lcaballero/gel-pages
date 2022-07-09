e6a4e83b192b86c3cae8e4039950cefa5127cf42
lucas <lucas@section.io>
2022-07-08 18:10:42 -0600

    Added more hydras and post draft writing.

--- .tools.el
+++ .tools.el
@@ -118,22 +118,79 @@ via .dir-locals "
   (interactive)
   (tools-run-command "run.sh" "lint"))
 
+(defun gel-region-to-para (beg end)
+  (interactive "r")
+  (save-excursion
+    (narrow-to-region beg end)
+    (set-mark nil)
+    (goto-char (point-min))
+    (insert "P.Text(`")
+    (goto-char (point-max))
+    (insert "`),")
+    (widen)))
+
+(defhydra org-gel-tools-hydra (:color pink :hint nil :exit t)
+  "
+^Org Gel^
+----------------------------------------------------------------------------------------------------------------
+_p_: region to gel para
+
+"
+  ("p" gel-region-to-para nil)
+
+  ("q" nil "quit" :exit t))
+
+(defhydra local-boilerplates (:color pink :hint nil :exit t)
+    "
+^Boilerplates^
+----------------------------------------------------------------------------------------------------------------
+_b_: bash file
+_d_: bash $DIR
+_h_: go http handler
+_t_: go test file
+
+"
+    ;; Boilerplates
+    ("b" tmpl-new-bash nil)
+    ("d" tmpl-script-dir nil)
+    ("h" tmpl-go-http-handler-func nil)
+    ("t" tmpl-go-new-test-file nil)
+    ;; exit
+    ("q" nil "quit" :exit t))
+
+(defhydra tools-gel-pages (:color pink :hint nil :exit t)
+  "
+^Dev Local^                 ^Deploy^
+----------------------------------------------------------------------------------------------------------------
+_d_: ./run.sh dist          _W_: ./deploy www
+_b_: ./run.sh build         _C_: ./deploy clean
+_c_: ./run.sh clean
+_t_: ./run.sh tests
+_l_: ./run.sh lint
+
+"
+  ;; local tooling for ./run.sh functions
+  ("d" tools-run-dist-read-later-net nil)
+  ("b" tools-run-build-read-later-net nil)
+  ("c" tools-run-clean-read-later-net nil)
+  ("t" tools-run-tests-read-later-net nil)
+  ("l" tools-run-lint-read-later-net nil)
+
+  ;; local tooling for ./deploy functions
+  ("W" tools-deploy-www-read-later-net nil)
+  ("C" tools-deploy-clean-read-later-net nil)
+
+  ("q" nil "quit" :exit t))
+
 (defhydra local-tools-hydra (:color pink :hint nil :exit t)
   "
-^Go Element (gel)^            ^Boilerplates^         ^Tools^                       ^Notes^
+^Go Element (gel)^            ^Tools^                       ^Notes^                     ^Sub Category^
 ----------------------------------------------------------------------------------------------------------------
-_i_: make Text() of region    _B_: bash file         _R_: reload local .tools.el   _r_: reload the notes file
-_p_: make P() tag of region   _D_: bash $DIR         _o_: open setup.org           _g_: link to line in file
-_m_: make XLink() of region   _H_: go http handler   _a_: run key/val to Add()
-_e_: make Code() of region    _T_: go test file      ^ ^
-^ ^                           ^ ^                    _w_: ./deploy www
-^ ^                           ^ ^                    _C_: ./deploy clean
-^ ^                           ^ ^                    ^ ^
-^ ^                           ^ ^                    _d_: ./run.sh dist
-^ ^                           ^ ^                    _b_: ./run.sh build
-^ ^                           ^ ^                    _c_: ./run.sh clean
-^ ^                           ^ ^                    _t_: ./run.sh tests
-^ ^                           ^ ^                    _l_: ./run.sh lint
+_i_: make Text() of region    _R_: reload local .tools.el   _r_: reload the notes file  _k_: org to gel hydra
+_p_: make P() tag of region   _o_: open setup.org           _g_: link to line in file   _b_: boilerplates hydra
+_m_: make XLink() of region   ^ ^                           ^ ^                         _t_: pages ./run or ./deploy hydra
+_e_: make Code() of region
+_a_: run key/val to Add()
 
 "
   ;; Go Element (gel)
@@ -141,24 +198,16 @@ _e_: make Code() of region    _T_: go test file      ^ ^
   ("p" region-to-p-tag nil)
   ("m" region-to-xlink-tag nil)
   ("e" region-to-code-tag nil)
-  ;; Boilerplates
-  ("B" tmpl-new-bash nil)
-  ("D" tmpl-script-dir nil)
-  ("H" tmpl-go-http-handler-func nil)
-  ("T" tmpl-go-new-test-file nil)
+  ("a" tools-edit-line nil)
+
+  ;; gel-region
+  ("k" org-gel-tools-hydra/body nil)
+  ("b" local-boilerplates/body nil)
+  ("t" tools-gel-pages/body nil)
+
   ;; Tools
   ("R" tools-load nil)
   ("o" open-setup-org nil)
-  ("a" tools-edit-line nil)
-  ;; --------------------------------------------
-  ("w" tools-deploy-www-read-later-net nil)
-  ("C" tools-deploy-clean-read-later-net nil)
-  ;; --------------------------------------------
-  ("d" tools-run-dist-read-later-net nil)
-  ("b" tools-run-build-read-later-net nil)
-  ("c" tools-run-clean-read-later-net nil)
-  ("t" tools-run-tests-read-later-net nil)
-  ("l" tools-run-lint-read-later-net nil)
 
   ;; Notes
   ("r" notes-reload nil)
@@ -166,4 +215,44 @@ _e_: make Code() of region    _T_: go test file      ^ ^
 
   ("q" nil "quit"))
 
+(global-unset-key (kbd "C-c C-v"))
 (global-set-key (kbd "C-c C-v") 'local-tools-hydra/body)
+(global-set-key (kbd "C-c v") 'local-tools-hydra/body)
+(global-set-key (kbd "C-c e") #'(lambda () (interactive) (eval-buffer)))
+
+(defun org-gel/parse-org-link ()
+  (interactive)
+  (let ((found nil)
+        (d 0))
+    (while (not found)
+      (let (
+            (props (cadr (org-element-link-parser)))
+            (begin (plist-get (cadr (org-element-link-parser)) :begin))
+            (end (plist-get (cadr (org-element-link-parser)) :end))
+            (contents-begin (plist-get (cadr (org-element-link-parser)) :contents-begin))
+            (contents-end (plist-get (cadr (org-element-link-parser)) :contents-end))
+            (raw-link (plist-get (cadr (org-element-link-parser)) :raw-link))
+            )
+        (setq d (+ d 1))        
+        ;;(message "point: %S, begin: %S, end: %S, depth: %d, props: %S" (point) begin end d props)
+        (if (or begin (= d 33))
+            (progn
+              (setq found t)
+              (let (
+                    (contents (buffer-substring-no-properties contents-begin contents-end))
+                    )
+                (delete-region begin end)
+                (insert (format "A.Atts(\"href\",\"%s\").Text(\"%s\")" raw-link contents))
+                ;;(message "contents: %s" contents)
+                ))
+          
+          (progn
+            (backward-word)
+            (backward-word)
+            (forward-word)
+            (forward-char 1)))))))
+
+;;org-element-link-parser 4103
+
+
+(global-set-key (kbd "C-c a") 'org-gel/parse-org-link)
